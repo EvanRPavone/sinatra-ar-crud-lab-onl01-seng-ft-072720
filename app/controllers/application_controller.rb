ec5c6c44cdabc2fde0315df290228561e9f925f5
Evan Pavone <evanrpavone@gmail.com>
2020-09-09 15:34:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/articles' do
-    @articles = Article.all
+    @article = Article.all
     erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
   end
 
 end
